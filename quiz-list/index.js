d8d636ede9460d1420071d7483387665a39ef649
Udon-japanese <odaniyoshito@gmail.com>
2023-07-01 12:17:15 +0900

    first commit

---
+++ quiz-list/index.js
@@ -0,0 +1,70 @@
+const storage = localStorage;
+
+const animalQuizId = crypto.randomUUID();
+const animalQuiz = {
+  id: animalQuizId,
+  name: "動物クイズ",
+  description: "みんな大好き動物に関するクイズです！",
+  questions: {
+    1: {
+      question: "パから始まってダで終わる白黒の動物は？",
+      choices: ["パソコンだ", "パンケーキだ", "パイナップルだ", "パンダ"],
+      correctAnswer: "パンダ",
+    },
+    2: {
+      question: "首が長い動物は？",
+      choices: ["人間", "ナマケモノ", "ゾウ", "キリン"],
+      correctAnswer: "キリン",
+    },
+    3: {
+      question: "あなたが好きな動物は？",
+      choices: ["サル", "サル", "サル", "サル"],
+      correctAnswer: "サル",
+    },
+  },
+};
+
+const humanQuizId = crypto.randomUUID();
+const humanQuiz = {
+  id: humanQuizId,
+  name: "人間クイズ",
+  description: "人間の体の構造に関するクイズです！",
+  questions: {
+    1: {
+      question: "人間の体を構成する骨の本数は？",
+      choices: ["約200本", "約2000本", "約20000本", "約200000本"],
+      correctAnswer: "約200本",
+    },
+    2: {
+      question: "高齢者の体に含まれる水分量は？",
+      choices: ["20%", "50%", "80%", "2%"],
+      correctAnswer: "50%",
+    },
+  },
+};
+
+storage.clear();
+storage.setItem(animalQuizId, JSON.stringify(animalQuiz));
+storage.setItem(humanQuizId, JSON.stringify(humanQuiz));
+
+const quizList = {};
+for (let i = 0; i < storage.length; i++) {
+  const key = storage.key(i);
+  const item = storage.getItem(key);
+  quizList[key] = JSON.parse(item);
+}
+
+console.log(quizList)
+
+const quizListContainer = document.createElement("div");
+for (const [quizId, quiz] of Object.entries(quizList)) {
+  const a = document.createElement("a");
+  const br = document.createElement("br");
+  a.innerText = quiz.name;
+  a.setAttribute("href", `/quiz/index.html?id=${quizId}`);
+  quizListContainer.appendChild(a);
+  quizListContainer.appendChild(br);
+}
+
+const container = document.getElementById("container");
+container.appendChild(quizListContainer);
---
+++ quiz/index.js
@@ -0,0 +1,86 @@
+const storage = localStorage;
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const quiz = JSON.parse(storage.getItem(id));
+console.log(quiz);
+
+const headerTemplate = `
+  <h1>{{title}}</h1>
+    <p>
+      <h3>{{description}}</h3>
+    </p>
+  <p>問題</p>`;
+const headerData = { title: quiz.name, description: quiz.description };
+const header = renderTemplate(headerTemplate, headerData);
+
+const quizContainer = document.createElement("div");
+for (const [n, question] of Object.entries(quiz.questions)) {
+  const shuffledChoices = shuffleChoices(question.choices);
+  const choices = renderChoices(shuffledChoices);
+
+  const qTemplate = `
+  <p>{{question}}
+    <br>
+    ${choices}
+    <p>正解：{{correctAnswer}}</p>
+  </p>`;
+  const qData = {
+    question: question.question,
+    correctAnswer: question.correctAnswer,
+  };
+  const q = renderTemplate(qTemplate, qData);
+  quizContainer.innerHTML += q;
+}
+
+const container = document.getElementById("container");
+container.innerHTML = header;
+container.appendChild(quizContainer);
+
+/**
+ * @description テンプレートとデータを受け取り、HTMLを生成する関数
+ * @param {string} template
+ * @param {object} data
+ * @returns {string} HTML文字列
+ */
+function renderTemplate(template, data) {
+  const keys = Object.keys(data);
+  let result = template;
+
+  keys.forEach((key) => {
+    const regex = new RegExp(`{{${key}}}`, "g");
+    result = result.replace(regex, data[key]);
+  });
+
+  return result;
+}
+
+/**
+ * @description 選択肢の順番をランダムに並び替える関数
+ * @param {string[]} choices 選択肢の配列
+ * @returns {string[]} ランダムに並び替えた配列
+ */
+function shuffleChoices(choices) {
+  for (let i = choices.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [choices[i], choices[j]] = [choices[j], choices[i]];
+  }
+  return choices;
+}
+
+/**
+ * @description 選択肢を選択肢の数だけ描画する関数
+ * @param {string[]} choices ランダムに並び替えた選択肢
+ * @returns {string} 選択肢のHTML文字列
+ */
+function renderChoices(choices) {
+  let result = "";
+  for (let i = 0; i < choices.length; i++) {
+    const choicesTemplate = "<li>{{c}}</li>";
+    const choicesData = { c: choices[i] };
+    const choice = renderTemplate(choicesTemplate, choicesData);
+    result += choice;
+  }
+
+  return `<ul>${result}</ul>`;
+}
